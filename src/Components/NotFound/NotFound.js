2275e27779b5b8571619f144eb3026633853e789
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-03 23:19:45 +0600

    component test

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>page Not Found</h3>
+        </div>
+    );
+};
+
+export default NotFound;<h3>page Not Found</h3>
