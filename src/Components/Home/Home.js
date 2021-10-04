4bb0602e51e63fd0fd60737ba712441a6cd05e0a
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-04 06:32:54 +0600

    why choose us done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import img from '../../images/banner-img.jpg'
+import './Home.css'
+import scholarship from '../../images/scholarship.png'
+import skilled from '../../images/skills.png';
+import service from '../../images/consult.png';
+import lecture from '../../images/lecture-sheet.png';
+
 const Home = () => {
     return (
         <Container className="text-white my-5">
@@ -15,15 +21,45 @@ const Home = () => {
                     <img className="img-fluid" src={img} alt="" />
                 </Col>
             </Row>
+            
             <Row className="my-5">
-                <Col md={6} className="d-flex justify-content-center align-items-center">
-                    <div >
-                        <h1>Academic Help Line</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi omnis aspernatur, tenetur quia veritatis repellat dolorem in, dicta odit doloribus eaque, commodi explicabo tempora animi illo aliquid dignissimos suscipit. Possimus.</p>
+                <h1 className='text-center'>Why Choose Us</h1>
+                
+                <Col md={6} className="">
+                    <div className="why-choose-us my-3 d-flex justify-content-center align-items-center ">
+                        <img className="img-fluid" src={scholarship} alt="" />
+                        <div className="mx-3">
+                            <h4>Scholarship Facilities</h4>
+                            <p>We provide scholarship opportunities for the bright students and also for the poor students.</p>
+                        </div>
                     </div>
                 </Col>
                 <Col md={6}>
-                    <img className="img-fluid" src={img} alt="" />
+                    <div className="why-choose-us my-3 d-flex justify-content-center align-items-center">
+                        <img className="img-fluid" src={skilled} alt="" />
+                        <div className="mx-3">
+                            <h4>Scholarship Facilities</h4>
+                            <p>We provide scholarship opportunities for the bright students and also for the poor students.</p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={6}>
+                    <div className="why-choose-us my-3 d-flex justify-content-center align-items-center">
+                        <img className="img-fluid" src={service} alt="" />
+                        <div className="mx-3">
+                            <h4>Scholarship Facilities</h4>
+                            <p>We provide scholarship opportunities for the bright students and also for the poor students.</p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={6}>
+                    <div className="why-choose-us my-3 d-flex justify-content-center align-items-center">
+                        <img className="img-fluid" src={lecture} alt="" />
+                        <div className="mx-3">
+                            <h4>Scholarship Facilities</h4>
+                            <p>We provide scholarship opportunities for the bright students and also for the poor students.</p>
+                        </div>
+                    </div>
                 </Col>
             </Row>
         </Container>
